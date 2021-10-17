560c4d06b5dc0c5fdda0b04d8bbc0301206c1be0
bkapovic <44833956+bkapovic@users.noreply.github.com>
2021-10-17 10:25:46 +0200

    Added 3 days worth of changes
    To do -> fix state on back key
              -> add loaders

---
+++ src/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import i18n from './i18n';
+
+ReactDOM.render(
+  
+    <App />,
+  
+  document.getElementById('root')
+);
+
